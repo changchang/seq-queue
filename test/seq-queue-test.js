e43c1916ac48dd5382a8e409dcf5c7cc311907b1
changchang <changchang005@gmail.com>
2012-03-16 18:39:17 +0800

    add ontimeout callback for queue.push and a return value for task.done to check whether timeout

--- test/seq-queue-test.js
+++ test/seq-queue-test.js
@@ -111,6 +111,7 @@ describe('seq-queue', function() {
 			var queue = SeqQueue.createQueue();
 			var executedTaskCount = 0;
 			var timeoutCount = 0;
+			var onTimeoutCount = 0;
 			//add timeout listener
 			queue.on('timeout', function(task) {
 				task.should.be.a('object');
@@ -121,6 +122,8 @@ describe('seq-queue', function() {
 			queue.push(function(task) {
 				executedTaskCount++;
 				//no task.done() invoke to cause a timeout
+			}, function() {
+				onTimeoutCount++;
 			}).should.be.true;
 			
 			queue.push(function(task) {
@@ -132,8 +135,43 @@ describe('seq-queue', function() {
 				//wait all task finish
 				executedTaskCount.should.be.equal(2);
 				timeoutCount.should.be.equal(1);
+				onTimeoutCount.should.be.equal(1);
 				done();
 			}, 4000);	//default timeout is 3s
+		});
+		
+		it('should return false when invoke task.done() if task has already timeout', function(done) {
+			var queue = SeqQueue.createQueue();
+			var executedTaskCount = 0;
+			var timeoutCount = 0;
+			var timeout = 1000;
+			
+			//add timeout listener
+			queue.on('timeout', function(task) {
+				task.should.be.a('object');
+				task.fn.should.be.a('function');
+				timeoutCount++;
+			});
+			
+			queue.push(function(task) {
+				executedTaskCount++;
+				task.done().should.be.true;
+			}).should.be.true;
+			
+			queue.push(function(task) {
+				//sleep to make a timeout
+				setTimeout(function() {
+					executedTaskCount++;
+					task.done().should.be.false;
+				}, timeout + 1000);
+			}, null, timeout).should.be.true;
+			
+			setTimeout(function() {
+				//wait all task finish
+				executedTaskCount.should.be.equal(2);
+				timeoutCount.should.be.equal(1);
+				done();
+			}, 4000);
 		});
 		
 		it('should never timeout after close forcefully', function(done) {
@@ -186,7 +224,7 @@ describe('seq-queue', function() {
 			
 			queue.push(function(task) {
 				//no task.done() invoke to cause a timeout
-			}, localTimeout).should.be.true;
+			}, null, localTimeout).should.be.true;
 			var start = Date.now();
 		});
 	});
